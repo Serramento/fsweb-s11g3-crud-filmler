41765dd8743164f2f0ad9e78fa3f07c7becd1324
Serramento <134259414+Serramento@users.noreply.github.com>
2023-10-15 11:24:23 +0300

    S11G3 tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,40 +1,64 @@
 import React, { useEffect, useState } from "react";
 
 import { Route, Switch, Redirect } from "react-router-dom";
-import MovieList from './components/MovieList';
-import Movie from './components/Movie';
+import MovieList from "./components/MovieList";
+import Movie from "./components/Movie";
 
-import MovieHeader from './components/MovieHeader';
+import MovieHeader from "./components/MovieHeader";
 
-import FavoriteMovieList from './components/FavoriteMovieList';
+import FavoriteMovieList from "./components/FavoriteMovieList";
 
-import axios from 'axios';
+import EditMovieForm from "./components/EditMovieForm";
+
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import AddMovieForm from "./components/AddMovieForm";
 
 const App = (props) => {
   const [movies, setMovies] = useState([]);
   const [favoriteMovies, setFavoriteMovies] = useState([]);
+  const history = useHistory();
+  const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
-    axios.get('http://localhost:9000/api/movies')
-      .then(res => {
+    axios
+      .get("http://localhost:9000/api/movies")
+      .then((res) => {
         setMovies(res.data);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }, []);
 
   const deleteMovie = (id) => {
-  }
+    axios
+      .delete(`http://localhost:9000/api/movies/${id}`)
+      .then((res) => {
+        setMovies(res.data);
+        history.push("/movies");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const addToFavorites = (movie) => {
+    const oldMovie = favoriteMovies.find((item) => item.id === movie.id);
+    if (!oldMovie) {
+      setFavoriteMovies([...favoriteMovies, movie]);
+    }
+  };
 
-  }
+  const handleDarkModeChange = () => {
+    setDarkMode(!darkMode);
+  };
 
   return (
-    <div>
-      <nav className="bg-zinc-800 px-6 py-3">
+    <div className={darkMode && "dark bg-slate-900 h-screen"}>
+      <nav className="bg-zinc-800 px-6 py-3 dark:bg-gray-800">
         <h1 className="text-xl text-white">HTTP / CRUD Film Projesi</h1>
+        <button onClick={handleDarkModeChange}>Dark Mode Aç/Kapa</button>
       </nav>
 
       <div className="max-w-4xl mx-auto px-3 pb-4">
@@ -44,10 +68,18 @@ const App = (props) => {
 
           <Switch>
             <Route path="/movies/edit/:id">
+              <EditMovieForm setMovies={setMovies} />
+            </Route>
+
+            <Route exact path="/movies/add">
+              <AddMovieForm setMovies={setMovies} />
             </Route>
 
             <Route path="/movies/:id">
-              <Movie />
+              <Movie
+                deleteMovie={deleteMovie}
+                addToFavorites={addToFavorites}
+              />
             </Route>
 
             <Route path="/movies">
@@ -64,6 +96,4 @@ const App = (props) => {
   );
 };
 
-
 export default App;
-
